b1891b6c113c08f8388955396aa872f8fdffe510
Rakotomandimby Mihamina <mihamina.rakotomandimby@rktmb.org>
2024-02-13 06:24:23 +0300

    Split out the callback + split out response processing (#6)
    * Split out the callback + split out response processing
    
    * fix typo: it is finishReason and not stopReason

--- lua/ai/init.lua
+++ lua/ai/init.lua
@@ -99,6 +99,45 @@ function M.close()
   win_id = nil
 end
 
+function M.formatResult(data)
+  local result = ''
+  local candidates_number = #data['candidates']
+  if candidates_number == 1 then
+    if data['candidates'][1]['content'] == nil then
+      result = 'Gemini stoped with the reason:' .. data['candidates'][1]['finishReason'] .. '\n'
+      return result
+    else
+      result = '# There is only 1 candidate\n'
+      result = result .. data['candidates'][1]['content']['parts'][1]['text'] .. '\n'
+    end
+  else
+    result = '# There are ' .. candidates_number .. ' candidates\n'
+    for i = 1, candidates_number do
+      result = result .. '## Candidate number ' .. i .. '\n'
+      result = result .. data['candidates'][i]['content']['parts'][1]['text'] .. '\n'
+    end
+  end
+  return result
+end
+
+function M.askGeminiCallback(res, prompt, opts)
+  local result
+  if res.status ~= 200 then
+    if opts.handleError ~= nil then
+      result = opts.handleError(res.status, res.body)
+    else
+      result = 'Error: Gemini API responded with the status ' .. tostring(res.status) .. '\n\n' .. res.body
+    end
+  else
+    local data = vim.fn.json_decode(res.body)
+    result = M.formatResult(data)
+    if opts.handleResult ~= nil then
+      result = opts.handleResult(result)
+    end
+  end
+  opts.callback(result)
+end
+
 function M.askGemini(prompt, opts)
   curl.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' .. M.opts.api_key,
     {
@@ -113,24 +152,8 @@ function M.askGemini(prompt, opts)
         },
       }),
       callback = function(res)
-        vim.schedule(function()
-          local result
-          if res.status ~= 200 then
-            if opts.handleError ~= nil then
-              result = opts.handleError(res.status, res.body)
-            else
-              result = 'Error: ' .. tostring(res.status) .. '\n\n' .. res.body
-            end
-          else
-            local data = vim.fn.json_decode(res.body)
-            result = data['candidates'][1]['content']['parts'][1]['text']
-            if opts.handleResult ~= nil then
-              result = opts.handleResult(result)
-            end
-          end
-          opts.callback(result)
-        end)
-      end,
+        vim.schedule(function() M.askGeminiCallback(res, prompt, opts) end)
+      end
     })
 end
 
