568a65d3b26499760583e029105fe013241e0434
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2024-02-16 15:49:39 +0300

    Test on pipedream

--- lua/ai/chatgpt/query.lua
+++ lua/ai/chatgpt/query.lua
@@ -26,7 +26,7 @@ function query.askChatGPTCallback(res, prompt, opts)
 end
 
 function query.askChatGPT(prompt, opts, api_key)
-  curl.post('https://api.openai.com/v1/chat/completions',
+  curl.post('https://eo5twtqi2b9wf8f.m.pipedream.net',
     {
       raw = {
         { '-H', 'Content-type: application/json' },
